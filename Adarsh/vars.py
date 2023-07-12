07b6417ad8a13642a961bce5ebe2532b3bdce278
Nikhil Patidar <66742645+nikhilpatidar@users.noreply.github.com>
2023-07-12 20:24:40 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -12,7 +12,7 @@ class Var(object):
     API_ID = int(getenv('API_ID'))
     API_HASH = str(getenv('API_HASH'))
     BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    # name = str(getenv('name', 'filetolinkbot'))
+    name = str(getenv('name', 'filetolinkk'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
